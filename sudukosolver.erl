0d9bc21a4442549fcc254c9fd053ac1f2f6aee51
Timothy John Wright <tjw@commsoft.co.uk>
2015-10-14 14:13:54 +0100

    12

--- sudukosolver.erl
+++ sudukosolver.erl
@@ -14,7 +14,7 @@ parse_board(binary_to_list(Bin));
 parse_board(Str) when is_list(Str) ->
 [list_to_integer(X)||X <- string:tokens(Str,"\r\n\t ")].
 
-create_grid_map([],10, Acc) ->
+create_grid_map([],_, Acc) ->
    lists:reverse(Acc);
 create_grid_map([B1, B2, B3, B4, B5, B6, B7, B8, B9|Rest], Y, Acc) ->
  List = [B1, B2, B3, B4, B5, B6, B7, B8, B9],
@@ -24,7 +24,9 @@ create_row([],X,Y,Acc) -> Acc;
 create_row([R|T], X, Y, Acc) ->
   create_row(T, X+1, Y, [{{X,Y}, R} | Acc]).
   
-
+  
+  
+%% some basic tests to ensure board is valid before starting
 test_Board(Board) when length(Board) == 81 ->
    "Ok";
 test_Board(Board) when length(Board) < 81 ->
