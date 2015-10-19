ad62ff2a77c785fe9e2317a6602785fac7624fea
Tim <wright.tim.j@gmail.com>
2015-10-19 23:53:10 +0100

    some issues, but pair matching sort of resolves some of evil board, but needs further review.  the pairs work fine for the normal board and resolve in 8 iterations of board

--- sudukosolver.erl
+++ sudukosolver.erl
@@ -14,7 +14,7 @@ main(File) ->
  Grids = get_3by3_grids(2, GridMap, []),
  sudukoutils:pretty_print(GridMap),
  Solution = solve(Grids, Grids, Rows, Columns, [], count_zeros(Grids, 0)),
- %%solve(Grids, Rows, Columns, [], count_zeros(Grids, 0)).
+ %%solve(Grids, Grids, Rows, Columns, [], count_zeros(Grids, 0)).
  %% need to covert grids to rows again.  annoying.
  sudukoutils:solution_pretty_print(Solution).
 
@@ -86,10 +86,6 @@ find_3by3_element(StartX,StartY,{{X,Y},_,_}) ->
  end. 
 
 
-%% filter grids by row
-filter_grids_by_row([], _, Filter) -> lists:reverse(Filter);
-filter_grids_by_row([CellX, CellY, CellZ|Tail], Grids, Filter) ->
- filter_grids_by_row(Tail, Grids, [filter_by_cell(Grids, CellX, [])|Filter]).
 
 %% filter list of lists by a cell
 filter_by_cell([], _, Filter) -> lists:reverse(Filter);
@@ -100,35 +96,56 @@ filter_by_cell([List|Tail], Cell, Filter) ->
   end.
 
     
-
 %% logic section.
-solve([],Grids, Rows,Columns, Solution, Counter)  -> 
+solve([], Grids, Rows,Columns, Solution, Counter)  -> 
  ZeroCounter = count_zeros(Solution, 0) ,
+ io:format("Zero counter ~w and Counter ~w ~n", [ZeroCounter, Counter]),
  if ZeroCounter < Counter -> 
-  solve(Solution, Grids, Rows, Columns,[], ZeroCounter);
-   true -> lists:reverse(Solution)
+  solve(lists:reverse(Solution), Grids, Rows, Columns,[], ZeroCounter);
+   true -> lists:reverse(Solution) 
  end;
-solve([Grid|Tail],Grids, Rows, Columns, Solution, Counter) -> 
+solve([Grid|Tail], Grids, Rows, Columns, Solution, Counter) -> 
 GridOptions = test_grids(Grid, Grid, Rows, Columns, []),
 UniqueValues = option_grid_solution_values(lists:seq(1,9),grid_solution_values(GridOptions, []),1,[]),
 Cell = find_option_cell(UniqueValues, GridOptions,[]),
+
+ if length(Cell) > 0 ->
+  {{X1,Y1},V1,F1} = hd(Cell),
+ io:format("unique solution {~w,~w} ~w ~n", [X1,Y1,V1]);
+ true -> io:format("no match ~n")
+end,
 
 UpdatedRows = update_maps(Rows, Cell, []),
 UpdatedColumns = update_maps(Columns, Cell, []),
 UpdatedGrid = update_map(Grid, Cell, []),
 
+%% need to update our grid, basically by replacing the updated grid and grid.
+UpdatedGrids = update_maps(Grids, Cell, []),
 
-PairValues = option_grid_solution_values(lists:seq(1,9),grid_solution_values(GridOptions, []),2,[]),
-Pairs = find_option_cell(PairValues, GridOptions, []),
+ PairGridOptions = test_grids(UpdatedGrid, UpdatedGrid, UpdatedRows, UpdatedColumns, []),
 
-%%if length(Pairs) > 2 ->
-%% Res = pair_evaluation(PairValues, Grids, UpdatedRows, UpdatedColumns, Pairs);
-%%true -> ok
-%%end,
+ PairValues = option_grid_solution_values(lists:seq(1,9),grid_solution_values(PairGridOptions, []),2,[]),
+ Pairs = find_option_cell(PairValues, PairGridOptions, []),
 
-solve(Tail,[UpdatedGrid|Tail], UpdatedRows, UpdatedColumns, [UpdatedGrid|Solution], Counter).
+ SolutionPairs = pair_evaluation(Pairs, UpdatedGrids, PairGridOptions, []),
 
-%% need to stop.  notes lost, but concept ok.  how to share the pair routine
+
+ if length(SolutionPairs) == 1 ->
+
+ if length(SolutionPairs) > 0 ->
+  {{X,Y},V,F} = hd(SolutionPairs),
+ io:format("pair solution {~w,~w} ~w ~n", [X,Y,V]);
+ true -> io:format("no match ~n")
+ end,
+
+ PairUpdatedRows = update_maps(UpdatedRows, SolutionPairs, []),
+ PairUpdatedColumns = update_maps(UpdatedColumns, SolutionPairs, []),
+ PairUpdatedGrid = update_map(UpdatedGrid, SolutionPairs, []),
+ PairUpdatedGrids = update_maps(UpdatedGrids, SolutionPairs, []),
+ solve(Tail,PairUpdatedGrids, PairUpdatedRows, PairUpdatedColumns, [PairUpdatedGrid|Solution], Counter);
+ true -> solve(Tail,UpdatedGrids, UpdatedRows, UpdatedColumns, [UpdatedGrid|Solution], Counter)
+end.
+
 
 %% function to calculate number of 0s in board
 count_zeros([], Counter) -> Counter;
@@ -143,23 +160,97 @@ count_grid_zeros([Grid|Tail], Counter) ->
  end.
 
 
-%% pair evaluation. see above.  return the cell we update, or nothing.
-pair_evaluation(_,_,_,_,[]) -> 0;
-%% this does not work.  but its nearly ready.  pairs can be more than 1 cell it seems.
-pair_evaluation(Pair, Grids, Rows, Columns, [{{CellX,CellX},_,_},{{PairCellX,PairCellY},_,_}]) ->
- Direction = "horizontal",
- if CellX == PairCellX -> Direction = "vertical";
-  true -> Direction = "horizontal"
- end,
+%% pair evaluation.  needs more checks but as long as we only return 1 field in the list (called recursively) then that field is good to use
+pair_evaluation([],_,_,Acc) -> Acc;
+pair_evaluation([{{CellX, CellY},V,F}, {{PairCellX,PairCellY},PV,PF}|Rest], Grids, GridOptions, Acc) ->
+ 
+ Direction = get_direction({{CellX, CellY},V,F}, {{PairCellX,PairCellY},PV,PF}),
+ CellList = lists:filter(fun(X) -> X /= V end, cell_solution_values(GridOptions,{{CellX, CellY},V,F}, [])),
+ PairCellList = lists:filter(fun(X) -> X /= PV end,cell_solution_values(GridOptions,{{PairCellX, PairCellY},PV,PF}, [])),
 
- %% so need direction 
- if Direction == "horizontal" -> ok;
- true -> ok
+ DirectionGrids = get_direction_grids(Grids, Direction, {CellX, CellY}, get_grid_centre(CellX), get_grid_centre(CellY), []),
+
+ UniqueOption = get_single_option(CellList, DirectionGrids, 0),
+ UniquePairOption = get_single_option(PairCellList, DirectionGrids, 0),
+
+%%logic is wrong, as its not checking all the combinations and therefore updates
+
+ io:format("{~w,~w} unique pair count ~w versus {~w,~w} option for value ~w ~w v ~w ~n", [CellX, CellY, UniquePairOption, PairCellX, PairCellY, UniqueOption, V, PV]),
+
+ if UniquePairOption /= 0, UniqueOption == 0 -> 
+      pair_evaluation(Rest, Grids,GridOptions,[{{CellX, CellY},V,"true"}|Acc]);
+    UniquePairOption == 0, UniqueOption /= 0 ->  
+      pair_evaluation(Rest, Grids, GridOptions, [{{PairCellX,PairCellY},PV,"true"}|Acc]);
+    true -> pair_evaluation(Rest, Grids,GridOptions, Acc)
+ end.
+
+get_single_option([], _, UniqueOption) -> UniqueOption;
+
+get_single_option([Option|T], Grids, UniqueOption) ->
+ %%io:format("option ~w ~n", [hd(Option)]),
+ CountOptions = count_options(Grids, Option, 0),
+ if CountOptions == 1 -> get_single_option(T, Grids, Option);
+  true -> get_single_option(T, Grids, 0)
+ end.
+
+count_options([], _, Count) -> Count;
+count_options([Grid|Tail], Option, Count) ->
+ Res = lists:any(fun({{X,Y},V,F}) -> V == Option end, Grid),
+
+ if Res == true -> count_options(Tail, Option, Count);
+  true -> count_options(Tail, Option, Count + 1)
+ end.
+
+
+get_direction({{PairCellX, PairCellY},_,_}, {{CellX, CellY},_,_}) ->
+ if CellX == PairCellX -> "horizontal";
+  true -> "vertical"
+ end.
+
+
+get_grid_centre(Val) ->
+ if Val < 4 -> 2;
+    Val > 3, Val < 7 -> 5;
+  true -> 8
+ end.
+
+get_direction_grids([],_, _,_,_, Acc) -> lists:reverse(Acc);
+get_direction_grids([Grid|T], Direction,Cell, CentreX, CentreY, Acc) ->
+ Filter = get_direction_grid(Grid, Direction, Cell, CentreX, CentreY),
+ if Filter == [] ->  get_direction_grids(T, Direction, Cell, CentreX, CentreY, Acc);
+  true -> 
+  get_direction_grids(T, Direction, Cell, CentreX, CentreY, [Filter|Acc])
+ end.
+
+get_direction_grid(Grid, Direction, Cell, CentreX, CentreY) ->
+ Filter = filter_this_direction_grid(Grid,fun({{X,Y},V,F}) -> {X,Y} == Cell end),
+ 
+ if Direction == "vertical" -> check_direction_grid(Filter, fun({{X,Y},V,F}) -> X == CentreX end);
+ true -> check_direction_grid(Filter, fun({{X,Y},V,F}) -> Y == CentreY end)
+ end.
+
+check_direction_grid(Grid, F) ->
+ Res = lists:any(F, Grid),
+ if Res == true -> Grid;
+ true -> []
+ end.
+
+filter_this_direction_grid(Grid, F) ->
+ Res = lists:any(F, Grid),
+ if Res == true -> [];
+ true -> Grid
  end.
 
 
 
-%%
+cell_solution_values([],_,Acc) -> lists:reverse(Acc);
+cell_solution_values([{{GX,GY},GV}|T],{{X,Y},V,F},Acc) ->
+ if GX == X, GY == Y -> cell_solution_values([],{{X,Y},V,F}, grid_solution_values([{{GX,GY},GV}], Acc));
+ true -> cell_solution_values(T,{{X,Y},V,F}, Acc)
+ end.
+
+
+%% check solutions options for the grid
 grid_solution_values([], Acc) -> lists:reverse(Acc);
 grid_solution_values([{{X,Y},V}|T], Acc) ->
  grid_solution_values(T, lists:append(V, Acc)).
@@ -168,7 +259,7 @@ grid_solution_values([{{X,Y},V}|T], Acc) ->
 option_grid_solution_values([],_,_, Acc) -> lists:reverse(Acc);
 option_grid_solution_values([Value|T], GridSolution, Option, Acc) ->
 List = lists:filter(fun(X) -> X == Value end, GridSolution),
-if length(List) == Option -> option_grid_solution_values(T, GridSolution, Option, lists:append(Acc, List));
+if length(List) == Option -> option_grid_solution_values(T, GridSolution, Option, lists:append(Acc, [Value]));
   true -> option_grid_solution_values(T, GridSolution, Option, Acc)
 end.
 
@@ -176,14 +267,14 @@ end.
 %% find the unique cell and update
 find_option_cell([],_, Acc) -> lists:reverse(Acc);
 find_option_cell([Unique|T], Grid, Acc) ->
-  find_option_cell(T, Grid, [find_grid_cell(Grid, Unique)|Acc]).
+  find_option_cell(T, Grid, find_grid_cell(Grid, Unique, Acc)).
 
 %% check each grid.
-find_grid_cell([], _) -> erlang:error("value not in cell");
-find_grid_cell([{{X,Y},V}|T], Value) ->  
+find_grid_cell([], _, Acc) -> lists:reverse(Acc);
+find_grid_cell([{{X,Y},V}|T], Value, Acc) ->  
  Res = lists:any(fun(X) -> X == Value end, V),
- if Res == true -> {{X,Y}, Value, "true"};
-  true -> find_grid_cell(T, Value)
+ if Res == true -> find_grid_cell(T, Value, [{{X,Y}, Value, "true"}|Acc]);
+  true -> find_grid_cell(T, Value, Acc)
   end.
 
 %% working fine now.
@@ -218,8 +309,6 @@ update_maps([Map|Rest], Solutions, Acc) ->
  update_maps(Rest, Solutions, [update_map(Map, Solutions, [])|Acc]).
  
 update_map([], _, Acc) -> lists:reverse(Acc);
-update_map({{X,Y},V,F}, Solutions, Acc) ->
- update_map([], Solutions,[update_solution(Solutions, {{X,Y},V,F})|Acc]);
 update_map([Map|T], Solutions, Acc) ->
  update_map(T, Solutions,[update_solution(Solutions, Map)|Acc]).
 
